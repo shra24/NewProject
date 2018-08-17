c5935c58ee0dbdf935c0bb3a800fc74a5460351e
shra24 <s,rane24@gmail.com>
2018-08-17 17:53:05 +0530

    NewProject

---
+++ src/DemoNew/MaxNum.java
@@ -0,0 +1,15 @@
+package DemoNew;
+
+public class MaxNum {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 34 ;
+		int b = 54 ;
+		
+		System.out.println(Math.min(a,b));
+		System.out.println(Math.max(a, b));
+
+}
+}
